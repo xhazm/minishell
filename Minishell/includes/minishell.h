4a069de527fb7e611ef184c1bb6d63d6abc4c9a3
Vidas Miseikis <vmiseiki@1-B-16.42heilbronn.de>
2022-01-17 18:02:43 +0100

    commit

--- Minishell/includes/minishell.h
+++ Minishell/includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: vmiseiki <vmiseiki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/15 20:21:26 by vmiseiki          #+#    #+#             */
-/*   Updated: 2022/01/16 23:33:20 by vmiseiki         ###   ########.fr       */
+/*   Updated: 2022/01/17 17:57:05 by vmiseiki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,13 @@
 
 typedef struct	s_cmd
 {
-	char	**arg;
-	int		length;
+	char	*cmd;
+	char 	*opt;
+	char 	*arg;
+	
 	int		type;
 	int		pipes[2];
+	struct s_cmd *next;
 }	t_cmd;
 
 #endif
--- Minishell/src/main.c
+++ Minishell/src/main.c
@@ -6,7 +6,7 @@
 /*   By: vmiseiki <vmiseiki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/13 16:56:43 by vmiseiki          #+#    #+#             */
-/*   Updated: 2022/01/17 13:52:41 by vmiseiki         ###   ########.fr       */
+/*   Updated: 2022/01/17 18:02:21 by vmiseiki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,18 +34,55 @@ char	*ft_str_join_c( char *str, char const c)
 		str = (char *) malloc (2 * sizeof(char));
 		if(!str)
 			return (NULL);
-		*str = *arr;
+		str[0] = arr[0];
+		str[1] = arr[1];
 		return (str);
 	}
 	return (ft_strjoin(str, arr));
 }
 
-void ft_split_input(char *input, int *i, int end)
+
+t_cmd	*ft_new_cmd()
+{
+	t_cmd *new;
+
+	new = (t_cmd *)malloc(sizeof(t_cmd));
+	if (!new)
+		return (NULL);
+	new->cmd = "test";
+	new->next = NULL;
+	return (new);
+}
+
+void	ft_generate_cmd(t_cmd **cmd)
+{
+	t_cmd	*new;
+
+	if ((*cmd) == NULL)
+		(*cmd)  = ft_new_cmd();
+	else
+	{
+		new = (*cmd);
+		(*cmd)  = ft_new_cmd();
+		(*cmd) -> next = new;
+	}
+}
+
+void	ft_check_bash_cmd(t_cmd *cmd)
+{
+	while(cmd->next !=  NULL)
+	{
+		printf("--->%s\n", cmd->cmd);
+		cmd = cmd->next;
+	}
+}
+
+void ft_split_input(char *input, int *i, int end, t_cmd **cmd)
 {
 	int j;
 	char *sub;
 	char flag;
-	char *cmd;
+	char *cpart;
 	
 	sub = ft_substr(input, (*i), end);
 	j = 0;
@@ -54,7 +91,7 @@ void ft_split_input(char *input, int *i, int end)
 	flag = 0;
 	while (sub[j] != '\0')
 	{
-		cmd = NULL;
+		cpart = NULL;
 		if(sub[j] != ' ')
 		{
 			flag = 1;
@@ -70,21 +107,23 @@ void ft_split_input(char *input, int *i, int end)
 						flag = 1;
 				}
 				if (flag != 0)
-					cmd = ft_str_join_c(cmd, sub[j]);
+					cpart = ft_str_join_c(cpart, sub[j]);
 				j++;
 			}
-			printf("%s\n", cmd);
-			free(cmd);
+			ft_generate_cmd(&(*cmd));
+			printf("%s\n", cpart);
+			free(cpart);
 		}
 		else
 			j++;
 	}
+	ft_check_bash_cmd(*cmd);
 	printf("---------\n");
 	(*i) = end;
 	free(sub);
 }
 
-void ft_validate_quotes(char *input)
+void ft_validate_quotes(char *input, t_cmd *cmd)
 {
 	int		i;
 	int		start;
@@ -102,7 +141,7 @@ void ft_validate_quotes(char *input)
 		else if (flag == 0 && (input[i] == '\'' || input[i] == '"'))
 			flag = (input[i]);
 		if (flag == 0 && (input[i + 1] == '|' || input[i + 1] == '\0'))
-			ft_split_input(input, &start, i + 1);
+			ft_split_input(input, &start, i + 1, &cmd);
 		i++;
 	}
 	if (flag != 0)
@@ -112,6 +151,7 @@ void ft_validate_quotes(char *input)
 int main ()
 {
 	char *input;
+	t_cmd	cmd;
 	int i;
 	
 	i = 0;
@@ -119,7 +159,7 @@ int main ()
 	{
 		if (ft_prompt(&input))
 		{
-			ft_validate_quotes(input);
+			ft_validate_quotes(input, &cmd);
 		}
 	}
 	//system("leaks minishell");fscanf(stdin, "c");
