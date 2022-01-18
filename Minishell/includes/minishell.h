9ee248eeffe0e6401dc47c622d1418848723dd44
Vidas Miseikis <vmiseiki@1-B-16.42heilbronn.de>
2022-01-18 21:09:57 +0100

    added function to store dara in to arrays. Some leeks

--- Minishell/includes/minishell.h
+++ Minishell/includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: vmiseiki <vmiseiki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/15 20:21:26 by vmiseiki          #+#    #+#             */
-/*   Updated: 2022/01/17 17:57:05 by vmiseiki         ###   ########.fr       */
+/*   Updated: 2022/01/18 20:59:37 by vmiseiki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,13 @@
 # include <readline/history.h>
 # include "../libft/libft.h"
 
+# define SINGLE_QUOTES = 0
+# define DOUBLE_QUOTES = 1
+
 typedef struct	s_cmd
 {
-	char	*cmd;
-	char 	*opt;
-	char 	*arg;
-	
+	int		argc;
+	char	**argv;
 	int		type;
 	int		pipes[2];
 	struct s_cmd *next;
--- Minishell/src/main.c
+++ Minishell/src/main.c
@@ -6,7 +6,7 @@
 /*   By: vmiseiki <vmiseiki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/13 16:56:43 by vmiseiki          #+#    #+#             */
-/*   Updated: 2022/01/17 18:21:03 by vmiseiki         ###   ########.fr       */
+/*   Updated: 2022/01/18 21:08:33 by vmiseiki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,6 @@ char	*ft_str_join_c( char *str, char const c)
 	return (ft_strjoin(str, arr));
 }
 
-
 t_cmd	*ft_new_cmd()
 {
 	t_cmd *new;
@@ -49,34 +48,29 @@ t_cmd	*ft_new_cmd()
 	new = (t_cmd *)malloc(sizeof(t_cmd));
 	if (!new)
 		return (NULL);
-	new->cmd = "test";
+	new->argc = 0;
+	new->argv = (char **)malloc(sizeof(char *));
+	if (!new->argv)
+		return (NULL);
+	new->argv[0] = NULL;
 	new->next = NULL;
 	return (new);
 }
 
 void	ft_generate_cmd(t_cmd **cmd)
 {
-	t_cmd	*new;
+	t_cmd	*tmp;
 
 	if ((*cmd) == NULL)
 		(*cmd)  = ft_new_cmd();
 	else
 	{
-		new = (*cmd);
-		(*cmd)  = ft_new_cmd();
-		(*cmd) -> next = new;
+		tmp = (*cmd);
+		while(tmp->next != NULL)
+			tmp = tmp->next;
+		tmp->next = ft_new_cmd();
 	}
 }
-
-void	ft_check_bash_cmd(t_cmd *cmd)
-{
-	while(cmd->next !=  NULL)
-	{
-		printf("--->%s\n", cmd->cmd);
-		cmd = cmd->next;
-	}
-}
-
 char *ft_handle_space_in_qoutes(char *sub, char *cpart, int *j, int flag)
 {
 
@@ -99,11 +93,55 @@ char *ft_handle_space_in_qoutes(char *sub, char *cpart, int *j, int flag)
 	return (cpart);
 }
 
-void ft_split_input(char *sub)
+int	ft_strlen2D(char **str)
+{
+	int	i;
+
+	i = 0;
+	while(str[i] != NULL)
+		i++;
+	return (i);
+}
+
+char	**ft_strdup2D(char **str1, char **str2)
+{
+	int	i;
+
+	i = 0;
+	while (str1[i] != NULL)
+	{
+		str2[i] = str1[i];
+		i++;
+	}
+	str2[i] = NULL;
+	return (str2);
+}
+
+int ft_store_cmd_argv(t_cmd **cmd, char *argv)
+{
+	
+	char	**tmp;
+
+	tmp = (char **)malloc(sizeof(char *) * ((*cmd)->argc));
+	if (!tmp)
+		return (0);
+	ft_strdup2D((*cmd)->argv, tmp);
+	free((*cmd)->argv);
+	(*cmd)->argv = (char **)malloc(sizeof(char *) * ((*cmd)->argc + 1));
+	if (!(*cmd)->argv)
+		return (0);
+	ft_strdup2D(tmp, (*cmd)->argv);
+	free(tmp);
+	(*cmd)->argv[(*cmd)->argc - 1] = ft_strdup(argv);
+	(*cmd)->argv[(*cmd)->argc] = NULL;
+	return (1);
+}
+
+void ft_split_input(char *sub, t_cmd **cmd)
 {
 	int	j;
 	char flag;
-	char *cpart;
+	char *argv;
 	
 	j = 0;
 	if (sub[j] == '|')
@@ -111,12 +149,13 @@ void ft_split_input(char *sub)
 	flag = 0;
 	while (sub[j] != '\0')
 	{
-		cpart = NULL;
+		argv = NULL;
 		if(sub[j] != ' ')
 		{
-			cpart = ft_handle_space_in_qoutes(sub, cpart, &j, flag);
-			ft_generate_cmd(&(*cmd), );
-			free(cpart);
+			argv = ft_handle_space_in_qoutes(sub, argv, &j, flag);
+			(*cmd)->argc++;
+			ft_store_cmd_argv(&(*cmd), argv);
+			free(argv);
 		}
 		else
 			j++;
@@ -126,38 +165,18 @@ void ft_split_input(char *sub)
 
 void ft_handle_input(char *input, int *i, int end, t_cmd **cmd)
 {
-	int j;
-	char *sub;			//substring of the input between pipes |
-	char flag; 			//flag for checking if spcae char is in the qoutes
-	char *cpart;		//comand line part extracted from substr
 
-	sub = ft_substr(input, (*i), end));
-	ft_handle_input(sub);
+	char	*sub;
+	
+	sub = ft_substr(input, (*i), end);
+	ft_generate_cmd(&(*cmd));
+	ft_split_input(sub, &(*cmd));
 
-	//sub = ft_substr(input, (*i), end);
-	// j = 0;
-	// if (sub[j] == '|')
-	// 	j++;
-	// flag = 0;
-	// while (sub[j] != '\0')
-	// {
-	// 	cpart = NULL;
-	// 	if(sub[j] != ' ')
-	// 	{
-	// 		cpart = ft_handle_space_in_qoutes(sub, cpart, &j, flag);
-	// 		ft_generate_cmd(&(*cmd), );
-	// 		free(cpart);
-	// 	}
-	// 	else
-	// 		j++;
-	// }
-	ft_check_bash_cmd(*cmd);
-	printf("---------\n");
 	(*i) = end;
 	free(sub);
 }
 
-void ft_validate_quotes(char *input, t_cmd *cmd)
+void ft_validate_quotes(char *input, t_cmd **cmd)
 {
 	int		i;
 	int		start;
@@ -175,27 +194,75 @@ void ft_validate_quotes(char *input, t_cmd *cmd)
 		else if (flag == 0 && (input[i] == '\'' || input[i] == '"'))
 			flag = (input[i]);
 		if (flag == 0 && (input[i + 1] == '|' || input[i + 1] == '\0'))
-			ft_handle_input(input, &start, i + 1, &cmd);
+			ft_handle_input(input, &start, i + 1, &(*cmd));
 		i++;
 	}
 	if (flag != 0)
 		printf("Error: argument\n");
 }
 
+void ft_check_struct(t_cmd *cmd)
+{
+	int i;
+	
+	while(cmd)
+	{
+		i = 0;
+		while(cmd->argv[i] != NULL)
+		{
+			printf("%s\n", cmd->argv[i]);
+			i++;
+		}
+		cmd = cmd -> next;
+	}
+}
+
+void ft_free_2D_arr(char **arr)
+{
+	int	i;
+	i = 0;
+	while(arr[i] != NULL)
+	{
+		free(arr[i]);
+		i++;
+	}
+	free(arr);
+}
+
+void ft_free_all(t_cmd **cmd)
+{
+	
+	t_cmd	*tmp;
+	
+	while((*cmd))
+	{
+		tmp = (*cmd);
+		ft_free_2D_arr(tmp->argv);
+		free(tmp);
+		(*cmd) = (*cmd)->next;
+	}
+	free((*cmd));
+}
+
 int main ()
 {
 	char *input;
-	t_cmd	cmd;
+	t_cmd	*cmd;
+
+	
 	int i;
 	
 	i = 0;
 	while (1)
 	{
+		cmd = NULL;
 		if (ft_prompt(&input))
 		{
 			ft_validate_quotes(input, &cmd);
+			ft_check_struct(cmd);
+			ft_free_all(&cmd);
+			//system("leaks minishell");fscanf(stdin, "c");
 		}
 	}
-	//system("leaks minishell");fscanf(stdin, "c");
 	return (0);
 }
