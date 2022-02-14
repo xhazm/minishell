cf3103d675e3eec85cf71366eca807f231691edf
Lars <lars.pfleiderfb@gmail.com>
2022-02-14 17:16:50 +0100

    execv now taking direct paths for cmd

--- Minishell/src/execv.c
+++ Minishell/src/execv.c
@@ -1,4 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execv.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpfleide <lpfleide@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/14 17:06:57 by lpfleide          #+#    #+#             */
+/*   Updated: 2022/02/14 17:15:58 by lpfleide         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/minishell.h"
+
+static int	ft_is_direct_path(t_cmd *cmd)
+{
+	char	*tmp;
+	int		len;
+
+	tmp = NULL;
+	len = ft_strlen(cmd->argv[0]);
+	if (access(cmd->argv[0], X_OK) == 0)
+	{
+		while(len > 0 && cmd->argv[0][len - 1] != '/')
+			len--;
+		tmp = cmd->argv[0];
+		cmd->argv[0] = ft_substr(cmd->argv[0], len, ft_strlen(cmd->argv[0]));
+		if (execv(tmp, cmd->argv) == -1)
+		{
+			ft_free(tmp);
+			return(ft_print_perrno(cmd->argv[0], tmp));
+		}
+		ft_free(tmp);
+		return (SUCCESS);
+	}
+	return (FAIL);
+}
 
 char	**ft_get_path(t_list **envp)
 {
@@ -28,6 +64,8 @@ int		ft_handle_execv(t_cmd	*cmd)
 
 	i = 0;
 	tmp = NULL;
+	if (ft_is_direct_path(cmd) == SUCCESS)
+		return (SUCCESS);
 	path = ft_get_path(ft_envp_pointer());
 	tmp = ft_strmapi(cmd->argv[0], ft_tolower);
 	while (path[i] != NULL)
